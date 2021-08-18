d04cc73086bcb0f9c07b4c7517584378d98b0421
VictorWeiLong <54586663+VittorioWeiLong@users.noreply.github.com>
2021-08-18 18:46:32 +0800

    input database environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA_bzSe5zt66gNGTqOGjXNhtf7IIfgjTUE",
+    authDomain: "synchrony-581ed.firebaseapp.com",
+    projectId: "synchrony-581ed",
+    storageBucket: "synchrony-581ed.appspot.com",
+    messagingSenderId: "457246691317",
+    appId: "1:457246691317:web:ad586375c8b8ee8d101abc",
+    measurementId: "G-YGYD6JY9D2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_bzSe5zt66gNGTqOGjXNhtf7IIfgjTUE",
+    authDomain: "synchrony-581ed.firebaseapp.com",
+    projectId: "synchrony-581ed",
+    storageBucket: "synchrony-581ed.appspot.com",
+    messagingSenderId: "457246691317",
+    appId: "1:457246691317:web:ad586375c8b8ee8d101abc",
+    measurementId: "G-YGYD6JY9D2"
+  }
 };
 
 /*
