8c59047374e0bb3193260236b1b73b228e73b7eb
simshengqin <sqsim.2018@sis.smu.edu.sg>
2021-08-20 11:10:11 +0800

    Add filter and firestore service

---
+++ src/app/core/services/filter.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import {FirestoreService} from './firestore.service';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FilterService {
+
+  constructor(
+    private fs: FirestoreService
+  ) {
+  }
+  getByDocId(collection: string, docId: string): Observable<any> {
+    return this.fs.doc$(collection + '/' + docId);
+  }
+  get(collection: string, filterName1: string = '', filterOp1: any = '', filterValue1: any = '',
+      filterName2: string = '', filterOp2: any = '', filterValue2: any = '',
+      filterName3: string = '', filterOp3: any = '', filterValue3: any = '',
+      filterName4: string = '', filterOp4: any = '', filterValue4: any = '',
+      sortColumn = '', isAsc = false): Observable<any[]> {
+    // Cannot use === as filterValue1 etc can be a number
+    // tslint:disable-next-line:triple-equals
+    console.log(filterValue1 + ',' + filterValue2 + ',' + filterValue3 + ',' + filterValue4 + ',');
+    // tslint:disable-next-line:triple-equals
+    if (filterValue1 == '' && filterValue2 == '' && filterValue3 == '' && filterValue4 == '') {
+      return this.fs.col$(collection, ref => {
+        return ref;
+        // .orderBy('createdDatetime', 'desc');
+      });
+    }
+    return this.fs.col$(collection, ref => {
+      let result;
+      if (filterValue1 !== '') {result = ref.where(filterName1, filterOp1, filterValue1); }
+      else if (filterValue2 !== '') {result = ref.where(filterName2, filterOp2, filterValue2); }
+      else if (filterValue3 !== '') {result = ref.where(filterName3, filterOp3, filterValue3); }
+      else if (filterValue4 !== '') {result = ref.where(filterName4, filterOp4, filterValue4); }
+      for (let i = 1; i <= 4; i++) {
+        switch (i) {
+          case 1:
+            if (filterValue1 !== '') {result = result.where(filterName1, filterOp1, filterValue1); }
+            break;
+          case 2:
+            if (filterValue2 !== '') {result = result.where(filterName2, filterOp2, filterValue2); }
+            break;
+          case 3:
+            if (filterValue3 !== '') {result = result.where(filterName3, filterOp3, filterValue3); }
+            break;
+          case 4:
+            if (filterValue4 !== '') {result = result.where(filterName4, filterOp4, filterValue4); }
+            break;
+        }
+      }
+      return result;
+    });
+  }
+
+}
---
+++ src/app/core/services/firestore.service.ts
@@ -0,0 +1,127 @@
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+import { Injectable } from '@angular/core';
+import {
+  Action, AngularFirestore, AngularFirestoreCollection, AngularFirestoreCollectionGroup,
+  AngularFirestoreDocument, DocumentChangeAction, DocumentSnapshotDoesNotExist,
+  DocumentSnapshotExists, QueryFn, QueryGroupFn
+} from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+type CollectionPredicate<T> = string | AngularFirestoreCollection<T>;
+type DocPredicate<T> = string | AngularFirestoreDocument<T>;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(private afs: AngularFirestore) {
+  }
+
+  createId(): string {
+    return this.afs.createId();
+  }
+
+  col<T>(ref: CollectionPredicate<T>, queryFn?: QueryFn): AngularFirestoreCollection<T> {
+    return typeof ref === 'string' ? this.afs.collection<T>(ref, queryFn) : ref;
+  }
+
+  colGroup<T>(collectionId: string, queryGroupFn?: QueryGroupFn<T>): AngularFirestoreCollectionGroup<T> {
+    return this.afs.collectionGroup<T>(collectionId, queryGroupFn);
+  }
+
+  doc<T>(ref: DocPredicate<T>): AngularFirestoreDocument<T> {
+    return typeof ref === 'string' ? this.afs.doc<T>(ref) : ref;
+  }
+
+  doc$<T>(ref: DocPredicate<T>): Observable<T> {
+    return this.doc(ref)
+      .snapshotChanges()
+      .pipe(
+        map((doc: Action<DocumentSnapshotDoesNotExist | DocumentSnapshotExists<T>>) => {
+          if (doc.payload.data()) {
+            return {...doc.payload.data(), docId: doc.payload.id} as T;
+          }
+          return null;
+        }),
+      );
+  }
+
+  col$<T>(ref: CollectionPredicate<T>, queryFn?: QueryFn): Observable<any[]> {
+    return this.col(ref, queryFn)
+      .snapshotChanges()
+      .pipe(
+        map((docs: DocumentChangeAction<T>[]) => {
+          return docs.map((a: DocumentChangeAction<T>) => {
+            const data = a.payload.doc.data() as T;
+            const docId = a.payload.doc.id;
+            return {...data, docId};
+          }) as T[];
+        }),
+      );
+  }
+
+  colGroup$<T>(collectionId: string, queryGroupFn?: QueryGroupFn): Observable<T[]> {
+    return this.colGroup(collectionId, queryGroupFn)
+      .snapshotChanges()
+      .pipe(
+        map((docs: DocumentChangeAction<T>[]) => {
+          return docs.map((a: DocumentChangeAction<T>) => {
+            const data = a.payload.doc.data() as T;
+            const docId = a.payload.doc.id;
+            return {...data, docId};
+          }) as T[];
+        }),
+      );
+  }
+
+  get timestamp() {
+    return firebase.firestore.FieldValue.serverTimestamp();
+  }
+
+  async set<T>(ref: DocPredicate<T>, data: any, options?: { merge: boolean }): Promise<void> {
+    const _merge: boolean = options?.merge ?? false;
+    await this.doc(ref).set(data, {merge: _merge});
+  }
+
+  async update<T>(ref: DocPredicate<T>, data: any): Promise<void> {
+    await this.doc(ref).update(data);
+  }
+
+  async delete<T>(ref: DocPredicate<T>): Promise<void> {
+    await this.doc(ref).delete();
+  }
+
+  add<T>(ref: CollectionPredicate<T>, data: any): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.col(ref).add(data).then((docRef) => {
+        return resolve(docRef.id);
+      }).catch(reject);
+    });
+  }
+
+  setOrAdd(path: string, data: any): Promise<any> {
+    const segments = path.split('/').filter(v => v);
+    if (segments.length % 2) {
+      // Odd is always a collection
+      return this.col(path).add(data);
+    } else {
+      // Even is always document
+      return this.doc(path).set(data);
+    }
+  }
+
+  upsert<T>(ref: DocPredicate<T>, data: any): Promise<void> {
+    const doc = this.doc(ref)
+      .snapshotChanges()
+      .pipe(take(1))
+      .toPromise();
+
+    return doc.then((snap: Action<DocumentSnapshotDoesNotExist | DocumentSnapshotExists<T>>) => {
+      return snap.payload.exists ? this.update(ref, data) : this.set(ref, data);
+    });
+  }
+}
