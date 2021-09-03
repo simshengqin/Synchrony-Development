bec3a2eeeb9d7f5aa194dfc309eeca8e8dbccbb8
simshengqin <54699127+simshengqin@users.noreply.github.com>
2021-09-03 11:37:15 +0800

    Added assignment-mark, assignment-mark-individual and videojs-record

---
+++ src/app/core/models/assignments_submissions_and_feedback.ts
@@ -0,0 +1,28 @@
+export interface AssignmentsSubmissionsAndFeedback {
+  docId?: string;
+  assignment_doc_id?: string;
+  instructor_doc_id?: string;
+  student_doc_id?: string;
+  school?: string;
+  school_instrument_level?: string;
+  submitted_datetime?: string;
+  student_attachment_scoresheet?: string;
+  student_attachment_scoresheet_name?: string;
+  student_attachment_recording?: string;
+  student_attachment_recording_name?: string;
+  feedback?: string;
+  instructor_feedback_attachment_name?: string,
+  instructor_feedback_attachment?: string;
+  feedback_datetime?: string;
+  grade?: number;
+  // Not in database, manually created
+  student?: Account;
+  instructor?: Account;
+}
+export enum Role {
+  admin = 'admin',
+  student = 'student',
+  instructor = 'instructor'
+  // freelancer = 'freelancer'
+}
+
