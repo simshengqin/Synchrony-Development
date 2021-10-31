e58ffa6191bdd84a925e85b19ec912afa90ecbb7
VictorWeiLong <54586663+VittorioWeiLong@users.noreply.github.com>
2021-10-31 17:39:26 +0800

    Bug fix on shared session
    Bug fix on shared session which cause the App to become unusable when user defocus e.g. reload the page or resuming using the page

--- src/app/core/services/sharedservice.service.ts
+++ src/app/core/services/sharedservice.service.ts
@@ -4,28 +4,33 @@ import { Observable, BehaviorSubject } from 'rxjs';
 @Injectable()
 export class SharedService {
 
-  private parameter; // parameter will contain values to be pass between URL or Componets.
-  private account!:any; // save account details when user login.
+  //private parameter; // parameter will contain values to be pass between URL or Componets.
+  //private account!:any; // save account details when user login.
 
   setComponentParameter(parameter) {
-    this.parameter = parameter;
+    //this.parameter = parameter;
+    sessionStorage.setItem('parameter', JSON.stringify(parameter));
   }
 
   getComponentParameter() {
-    return this.parameter;
+    //return this.parameter;
+    return JSON.parse(sessionStorage.getItem("parameter"));
   }
 
   setAccount(account:any){
-    this.account = JSON.stringify(account)
+    //this.account = JSON.stringify(account)
+    sessionStorage.setItem('account', JSON.stringify(account));
   }
 
   getAccount(){
-    return JSON.parse(this.account)
+    //return JSON.parse(this.account)
+    return JSON.parse(sessionStorage.getItem("account"));
   }
 
   reset(){
-    this.account = null;
-    this.parameter = "";
+    //this.account = null;
+    //this.parameter = "";
+    sessionStorage.clear()
   }
 
 }
