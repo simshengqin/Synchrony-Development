3b88e5dc079425db3658396cb91677173d6322c2
simshengqin <54699127+simshengqin@users.noreply.github.com>
2021-10-31 12:55:51 +0800

    Fix: Instructor cannot add students that are not in his class or school +  if the username already exists and the role column is changed for a particular row, show an error message

--- src/app/ui/admin/account-create/account-create.component.ts
+++ src/app/ui/admin/account-create/account-create.component.ts
@@ -7,6 +7,7 @@ import {Account, Role} from '../../../../app/core/models/account';
 import * as bcrypt from 'bcryptjs';
 import {ActivatedRoute, Router} from "@angular/router";
 import {ConfirmModalComponent} from "../../../shared/components/confirm-modal/confirm-modal.component";
+import {SharedService} from "../../../core/services/sharedservice.service";
 
 @Component({
   selector: 'app-account-create',
@@ -24,15 +25,19 @@ export class AccountCreateComponent implements OnInit {
   isUploading = false;
   isAdmin: boolean;
   @ViewChild(ConfirmModalComponent) confirmModalComponent: ConfirmModalComponent;
+  loggedInAccount: Account;
   constructor(
     private toastrService: ToastrService,
     private ngxCsvParser: NgxCsvParser,
     private crudService: CrudService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private sharedService: SharedService,
   ) {}
   async ngOnInit(): Promise<void> {
-    this.isAdmin = this.router.url === '/admin/account/create';
+    this.loggedInAccount = JSON.parse(this.sharedService.getAccount());
+    console.log(this.loggedInAccount);
+    this.isAdmin = this.loggedInAccount.role === 'admin';
     const singleAccount: Account = await this.crudService.readByDocId('accounts', '7hQyZTken7p6eSAR8MQB')
       .pipe(first())
       .toPromise();
@@ -124,8 +129,24 @@ export class AccountCreateComponent implements OnInit {
               if (this.isAdmin && !['admin', 'instructor', 'student'].includes(account.role)) {
                 this.errors.push('Row ' + i + ' has illegal values for role column (Only admin, instructor or student is accepted)');
               }
-              if (!this.isAdmin && account.role !== 'student') {
-                this.errors.push('Row ' + i + ' has illegal values for role column (Only student is accepted)');
+              if (!this.isAdmin) {
+                if (account.role !== 'student') { this.errors.push('Row ' + i + ' has illegal values for role column (Only student is accepted)'); }
+                const invalidSchoolInstrumentLevels  = [];
+                for (const schoolInstrumentLevel of account.school_instrument_level) {
+                  let exist = false;
+                  for (const instructorSchoolInstrumentLevel of this.loggedInAccount.school_instrument_level) {
+                    if (schoolInstrumentLevel === instructorSchoolInstrumentLevel) { exist = true; }
+                  }
+                  if (!exist) { invalidSchoolInstrumentLevels.push(schoolInstrumentLevel); }
+                }
+                if (invalidSchoolInstrumentLevels.length > 0) {
+                  this.errors.push('Row ' + i + ' has illegal values for school_instrument_level column. Instructors can only upload students with the same school_instrument_level as them. ' +
+                    'Invalid values: ' + invalidSchoolInstrumentLevels.join(', '));
+                }
+                if (account.role !== 'student') { this.errors.push('Row ' + i + ' has illegal values for role column (Only student is accepted)'); }
+              }
+              if (accounts.length > 0 && account.role !== accounts[0].role) {
+                this.errors.push('Row ' + i + ' has illegal values for role column (Cannot change role for existing users)');
               }
             }
             // if (accounts.length > 0) {
@@ -134,6 +155,7 @@ export class AccountCreateComponent implements OnInit {
             if (account.password.length > 0 && account.password.length < 5) {
               this.errors.push('Row ' + i + ' does not meet the minimum length requirement for password (5)');
             }
+
             // Stop the creation/updating of ALL accounts as long as there is a problem with 1 of the account
             if (this.errors.length === 0) {
               await this.crudService.create('accounts', account); // .then(r => {const ownerDocId = r; } );
@@ -141,7 +163,7 @@ export class AccountCreateComponent implements OnInit {
                 // console.log(account);
                 await this.crudService.create('accounts', account); // .then(r => {const ownerDocId = r; } );
               } else {
-                await this.crudService.update('accounts', accounts[0].docId, account);
+                  await this.crudService.update('accounts', accounts[0].docId, account);
               }
             }
             i++;
