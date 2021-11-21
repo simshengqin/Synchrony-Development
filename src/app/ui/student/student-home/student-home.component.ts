9d91183afe202875610f7b97c8ef2981c48414ae
VictorWeiLong <54586663+VittorioWeiLong@users.noreply.github.com>
2021-11-21 17:52:14 +0800

    Add security access to student home component

--- src/app/ui/student/student-home/student-home.component.ts
+++ src/app/ui/student/student-home/student-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/core/services/sharedservice.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-home',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentHomeComponent implements OnInit {
 
-  constructor() { }
+  security_role_access: string = "student";
+
+  constructor(
+    private sharedService:SharedService,
+    private router: Router,
+    private toastrService: ToastrService,
+  ) { }
 
   ngOnInit(): void {
+    if(JSON.parse(this.sharedService.getAccount()) != null){
+      var student  = JSON.parse(this.sharedService.getAccount());
+      if(this.security_role_access != student.role){
+        this.router.navigate(['/login']);
+        this.toastrService.error('Access denied invalid user access detect!', '', {positionClass: 'toast-top-center'});
+      }
+    } else{
+      this.router.navigate(['/login']);
+    }
   }
 
 }
